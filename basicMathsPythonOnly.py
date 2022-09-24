de4a7d6280857c71008ccaa2e2b2f85526391572
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2022-09-24 14:20:35 -0500

    Update basicMathsPythonOnly.py

--- basicMathsPythonOnly.py
+++ basicMathsPythonOnly.py
@@ -1,5 +1,20 @@
 total_sample_size = 3000000
 max_val = 10000
 
+from optparse import Values
 import random
+from tkinter import Y
+from unittest import result
 
+#########################_______ GENERATE DATA FOR ADDITION, SUBTRACTION, MULTIPLICATION AND DIVISION_________########################
+# 1. generate two Values
+# 2. symbol is +,-,*,/
+# 3. str_in = x symb Y
+# 4. str out is result
+# 5. write to file but not in method
+
+def generate_math_pairs(operation):
+    x = random.randrange(1,max_val)
+    y = random.randrange(1,max_val)
+
+    if operation == 'add':
